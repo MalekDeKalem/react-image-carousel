7e871553a83d37143d5446c9b9ef2068d37e8671
Malik <dimitri200169@gmail.com>
2023-03-08 01:27:46 +0100

    little image carousel with cat images

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import  { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [current, setCurrent] = useState(0);
+
+  const images = [
+    "./images/rcat1.jpg",
+    "./images/rcat2.jpg",
+    "./images/rcat3.jpg",
+    "./images/rcat4.jpg",
+    "./images/rcat5.jpg",
+    "./images/rcat6.jpg",
+    "./images/rcat7.jpg",
+    "./images/rcat8.jpg"
+  ];
+
+  const prevImage = () => {
+    setCurrent(current === 0 ? images.length - 1 : current - 1);
+  }
+
+  const nextImage = () => {
+    setCurrent(current === images.length - 1 ? 0 : current + 1);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1 className='my-title'> Demo of an Image Carousel </h1>
+      <div>
+
+        <div>
+          {images.map((image, index) => 
+            current === index && (
+              <div key={image} className="image-container">
+                <div className="left-ball" onClick={prevImage}> <h5 className="arrow"> &#8592; </h5> </div>
+                <img src={require(`${image}`)} alt="image"/>
+                <div className="right-ball" onClick={nextImage}> <h5 className="arrow"> &#8594; </h5> </div>
+              </div>
+            )
+          )}
+        </div>
+      </div>
     </div>
+    
   );
 }
 
